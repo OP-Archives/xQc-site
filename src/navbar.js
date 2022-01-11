118cf36dc6615df893e588e4a8b2a31a92d42030
TimIsOverpowered <admin@angelthump.com>
2022-01-11 15:52:05 -0600

    navbar work

--- src/navbar.js
+++ src/navbar.js
@@ -1,163 +1,77 @@
 import React from "react";
-import { AppBar, Toolbar, List, ListItem, ListItemText, Typography, Divider, IconButton, Drawer, useMediaQuery, Box } from "@mui/material";
-import Logo from "./assets/loading.gif";
-import Menu from "@mui/icons-material/Menu";
-import discord from "./assets/discord.png";
-import twitter from "./assets/twitter.png";
-import soundcloud from "./assets/soundcloud.png";
-import reddit from "./assets/reddit.png";
-import youtube from "./assets/youtube.png";
-import twitch from "./assets/twitch.png";
+import { AppBar, Toolbar, Box, Typography, InputBase } from "@mui/material";
+import Logo from "./assets/logo.png";
 import CustomLink from "./utils/CustomLink";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
 
-const mainLinks = [
-  { title: `Home`, path: `/` },
-  { title: `Vods`, path: `/vods` },
-  { title: `Contest`, path: `/contest` },
-  { title: `Merch`, path: `https://metathreads.com/collections/pokelawls` },
-];
-const socialLinks = [
-  { title: `Twitch`, path: `https://twitch.tv/pokelawls` },
-  { title: `Twitter`, path: `https://twitter.com/pokelawls` },
-  { title: `Reddit`, path: `https://reddit.com/r/pokelawls` },
-  { title: `Discord`, path: `https://discord.gg/pokelawls` },
-  { title: `Youtube`, path: `https://youtube.com/c/pokelawls` },
-  { title: `Soundcloud`, path: `https://soundcloud.com/pokelawls` },
-];
-
-export default function Navbar(props) {
-  const isMobile = useMediaQuery("(max-width: 800px)");
-
-  const [drawerOpen, setDrawerOpen] = React.useState(false);
-  const handleDrawerToggle = () => {
-    setDrawerOpen(!drawerOpen);
-  };
-
-  const drawer = (
-    <div>
-      <Box sx={{ alignItems: "stretch", justifyContent: "center", display: "flex", flexShrink: 1, flexGrow: 1, mb: 1, mt: 1 }}>
-        <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <a href="/">
-            <img alt="" height="45px" src={Logo} />
-          </a>
-        </Box>
-      </Box>
-      <Divider />
-      <List>
-        {mainLinks.map(({ title, path }) => (
-          <ListItem key={title} component={CustomLink} href={path}>
-            <ListItemText primary={title} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {socialLinks.map(({ title, path }) => (
-          <ListItem key={title} component={CustomLink} href={path} target="_blank" rel="noreferrer noopener">
-            <ListItemText primary={title} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-    </div>
-  );
-
-  return isMobile ? (
-    <Box sx={{ display: "flex" }}>
-      <AppBar position="static" color="inherit">
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
         <Toolbar>
-          <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle}>
-            <Menu />
-          </IconButton>
-          <Box sx={{ alignItems: "stretch", justifyContent: "center", display: "flex", flexShrink: 1, flexGrow: 1, mb: 1, mt: 1 }}>
-            <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-              <a href="/">
-                <img alt="" height="45px" src={Logo} />
-              </a>
-            </Box>
+          <Box sx={{ mr: 2 }}>
+            <a href="/">
+              <img alt="" style={{ maxWidth: "45px", height: "auto" }} src={Logo} />
+            </a>
           </Box>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <CustomLink color="inherit" href="/">
+              xQc Vods
+            </CustomLink>
+          </Typography>
+
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
+          </Search>
         </Toolbar>
       </AppBar>
-      <nav aria-label="navigation">
-        <Drawer
-          open={drawerOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true,
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-    </Box>
-  ) : (
-    <Box sx={{ display: "flex", backgroundColor: "#1d1d1d", alignItems: "stretch", flexWrap: "nowrap" }}>
-      <Box sx={{ alignItems: "stretch", justifyContent: "flex-start", width: "100%", display: "flex", flexShrink: 1, flexGrow: 1, flexWrap: "nowrap" }}>
-        <Box sx={{ ml: 1, display: "flex", justifyContent: "space-between", flexDirection: "row", height: "100%" }}>
-          <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", mr: 0.5 }}>
-            <a href="https://twitter.com/pokelawls" target="_blank" rel="noopener noreferrer">
-              <img alt="" height="auto" width="35px" src={twitter} />
-            </a>
-          </Box>
-          <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", mr: 0.5 }}>
-            <a href="https://discord.gg/pokelawls" target="_blank" rel="noopener noreferrer">
-              <img alt="" height="auto" width="45px" src={discord} />
-            </a>
-          </Box>
-          <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", mr: 0.5 }}>
-            <a href="https://soundcloud.com/pokelawls" target="_blank" rel="noopener noreferrer">
-              <img alt="" height="auto" width="45px" src={soundcloud} />
-            </a>
-          </Box>
-          <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", mr: 0.5 }}>
-            <a href="https://reddit.com/r/pokelawls" target="_blank" rel="noopener noreferrer">
-              <img alt="" height="auto" width="42px" src={reddit} />
-            </a>
-          </Box>
-          <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center", mr: 0.5 }}>
-            <a href="https://youtube.com/c/pokelawls" target="_blank" rel="noopener noreferrer">
-              <img alt="" height="auto" width="45px" src={youtube} />
-            </a>
-          </Box>
-        </Box>
-      </Box>
-      <Box sx={{ alignItems: "center", display: "flex", flexGrow: 1, flexShrink: 1, width: "100%", justifyContent: "center" }}>
-        <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <a href="/">
-            <img alt="" height="45px" src={Logo} />
-          </a>
-        </Box>
-      </Box>
-      <Box sx={{ alignItems: "center", display: "flex", flexGrow: 1, flexShrink: 1, width: "100%", justifyContent: "flex-end", mr: 1 }}>
-        <Box sx={{ display: "flex", mr: 1 }}>
-          <CustomLink href="/contest">
-            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-              <Box sx={{ ml: 1 }}>
-                <Typography>Contest</Typography>
-              </Box>
-            </Box>
-          </CustomLink>
-        </Box>
-        <Box sx={{ display: "flex", mr: 1 }}>
-          <CustomLink href="/vods">
-            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-              <Box sx={{ ml: 1 }}>
-                <Typography>Vods</Typography>
-              </Box>
-            </Box>
-          </CustomLink>
-        </Box>
-        <Box sx={{ display: "flex", ml: 1, mr: 1 }}>
-          <CustomLink href="https://twitch.tv/pokelawls" target="_blank" rel="noopener noreferrer">
-            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-              <img alt="" height="auto" width="38px" src={twitch} />
-              <Box sx={{ ml: 1 }}>
-                <Typography>Watch me Live</Typography>
-              </Box>
-            </Box>
-          </CustomLink>
-        </Box>
-      </Box>
     </Box>
   );
 }
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -1,17 +1,23 @@
 import { styled, Typography } from "@mui/material";
 import CustomLink from "./CustomLink";
+import xqcL from "../assets/xqcL.png";
 
 const Footer = styled((props) => (
   <div {...props}>
     <div style={{ marginTop: "0.5rem" }}>
       <Typography variant="caption" color="textSecondary">
-        {`Pokelawls © ${new Date().getFullYear()}`}
+        {`xQc © ${new Date().getFullYear()}`}
       </Typography>
     </div>
     <CustomLink href="https://twitter.com/overpowered">
-      <Typography variant="caption" color="textSecondary">
-        made by OP with 💜
-      </Typography>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <Typography variant="caption" color="textSecondary">
+          made by OP with
+        </Typography>
+        <div style={{ marginLeft: "6px" }}>
+          <img alt="" src={xqcL} height="24" width="24" />
+        </div>
+      </div>
     </CustomLink>
   </div>
 ))`
