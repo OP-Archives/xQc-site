6114ba4ae3ce066ee59a48c6f333255190744901
TimIsOverpowered <admin@angelthump.com>
2022-05-30 20:06:39 -0500

    new tab for socials

--- src/navbar.js
+++ src/navbar.js
@@ -115,12 +115,12 @@ export default function NavBar() {
           </Box>
           <Box sx={{ display: "flex", justifyContent: "end", flex: 1 }}>
             <Box sx={{ mr: 2 }}>
-              <a href="https://twitter.com/xqc">
+              <a href="https://twitter.com/xqc" rel="noopener noreferrer" target="_blank">
                 <TwitterIcon />
               </a>
             </Box>
             <Box sx={{ mr: 2 }}>
-              <a href="https://twitch.tv/xqc">
+              <a href="https://twitch.tv/xqc" rel="noopener noreferrer" target="_blank">
                 <SvgIcon>
                   <path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z" />
                 </SvgIcon>
